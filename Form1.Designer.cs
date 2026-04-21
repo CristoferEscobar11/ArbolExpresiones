712f1270ad7befd1ead326cc0119e623e621a92c
crise <cris.escobar777@gmail.com>
2026-04-20 21:58:16 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ArbolExpresiones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Location = new System.Drawing.Point(88, 54);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(180, 22);
+            this.txtExpresion.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(88, 97);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(180, 56);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar Arbol";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(88, 179);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(634, 259);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtExpresion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExpresion;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArbolExpresiones
+{
+    public partial class Form1 : Form
+    {
+        Nodo raiz;
+        List<NodoVisual> nodosAnimados = new List<NodoVisual>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnGenerar.Click += btnGenerar_Click;
+            panel1.Paint += panel1_Paint;
+
+            panel1.BackColor = Color.White;
+        }
+
+      
+        private async void btnGenerar_Click(object sender, EventArgs e)
+        {
+            string infija = txtExpresion.Text.Replace(" ", "");
+            string postfija = InfijaAPostfija(infija);
+
+            raiz = ConstruirArbol(postfija);
+
+            nodosAnimados.Clear();
+            await GenerarAnimacion(raiz, panel1.Width / 2, 20, 120);
+        }
+
+        
+        public class Nodo
+        {
+            public string Valor;
+            public Nodo Izquierdo;
+            public Nodo Derecho;
+
+            public Nodo(string valor)
+            {
+                Valor = valor;
+            }
+        }
+
+        
+        public class NodoVisual
+        {
+            public Nodo Nodo;
+            public int X, Y;
+            public int TargetX, TargetY;
+        }
+
+       
+        public static int Prioridad(char op)
+        {
+            if (op == '+' || op == '-') return 1;
+            if (op == '*' || op == '/') return 2;
+            return 0;
+        }
+
+      
+        public static string InfijaAPostfija(string exp)
+        {
+            Stack<char> pila = new Stack<char>();
+            string salida = "";
+
+            foreach (char c in exp)
+            {
+                if (char.IsDigit(c)) salida += c;
+                else if (c == '(') pila.Push(c);
+                else if (c == ')')
+                {
+                    while (pila.Peek() != '(')
+                        salida += pila.Pop();
+                    pila.Pop();
+                }
+                else
+                {
+                    while (pila.Count > 0 && Prioridad(pila.Peek()) >= Prioridad(c))
+                        salida += pila.Pop();
+                    pila.Push(c);
+                }
+            }
+
+            while (pila.Count > 0)
+                salida += pila.Pop();
+
+            return salida;
+        }
+
+      
+        public static Nodo ConstruirArbol(string postfija)
+        {
+            Stack<Nodo> pila = new Stack<Nodo>();
+
+            foreach (char c in postfija)
+            {
+                if (char.IsDigit(c)) pila.Push(new Nodo(c.ToString()));
+                else
+                {
+                    Nodo der = pila.Pop();
+                    Nodo izq = pila.Pop();
+
+                    Nodo nuevo = new Nodo(c.ToString());
+                    nuevo.Izquierdo = izq;
+                    nuevo.Derecho = der;
+
+                    pila.Push(nuevo);
+                }
+            }
+            return pila.Pop();
+        }
+
+        
+        private async Task GenerarAnimacion(Nodo nodo, int x, int y, int espacio)
+        {
+            if (nodo == null) return;
+
+            NodoVisual nv = new NodoVisual()
+            {
+                Nodo = nodo,
+                X = panel1.Width / 2,
+                Y = 0,
+                TargetX = x,
+                TargetY = y
+            };
+
+            nodosAnimados.Add(nv);
+
+            for (int i = 0; i < 20; i++)
+            {
+                nv.X += (nv.TargetX - nv.X) / 5;
+                nv.Y += (nv.TargetY - nv.Y) / 5;
+                panel1.Invalidate();
+                await Task.Delay(20);
+            }
+
+            await GenerarAnimacion(nodo.Izquierdo, x - espacio, y + 80, espacio / 2);
+            await GenerarAnimacion(nodo.Derecho, x + espacio, y + 80, espacio / 2);
+        }
+
+       
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            foreach (var nv in nodosAnimados)
+            {
+                if (nv.Nodo.Izquierdo != null)
+                {
+                    var hijo = nodosAnimados.Find(n => n.Nodo == nv.Nodo.Izquierdo);
+                    if (hijo != null)
+                        g.DrawLine(Pens.Gray, nv.X + 15, nv.Y + 30, hijo.X + 15, hijo.Y);
+                }
+
+                if (nv.Nodo.Derecho != null)
+                {
+                    var hijo = nodosAnimados.Find(n => n.Nodo == nv.Nodo.Derecho);
+                    if (hijo != null)
+                        g.DrawLine(Pens.Gray, nv.X + 15, nv.Y + 30, hijo.X + 15, hijo.Y);
+                }
+            }
+
+            foreach (var nv in nodosAnimados)
+            {
+                g.FillEllipse(Brushes.LightSkyBlue, nv.X, nv.Y, 30, 30);
+                g.DrawEllipse(Pens.Black, nv.X, nv.Y, 30, 30);
+                g.DrawString(nv.Nodo.Valor, this.Font, Brushes.Black, nv.X + 10, nv.Y + 5);
+            }
+        }
+    }
+}
